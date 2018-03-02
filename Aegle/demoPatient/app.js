3d59ecb0da67f2c1188a4cbeb96fba54056b6b9c
Paulo Pereira <palbp.giga@gmail.com>
2018-03-02 18:20:54 +0000

    A implementação foi documentada e é usado I/O assíncrono

--- Aegle/demoPatient/app.js
+++ Aegle/demoPatient/app.js
@@ -1,28 +1,66 @@
-const fs = require('fs')
-const patientFactory = require('./patient.js')
 
-const configFilePath = process.argv[2]
+ /**
+ * Entry point that starts a set of patients according to the info contained in the given config file.
+ * The full path of the configuration file is specified as a command line argument. The configuration 
+ * is specified using JSON as literal of @type {PatientConfig[]}
+ * 
+ * @module demoPatient.main
+ */
 
-if (!configFilePath || !fs.existsSync(configFilePath)) {
-    console.error('Usage: node app <configFilePath>')
-    return
+main(process.argv[2])
+
+ /** 
+  * Data type that holds configuration information to be used in initializing patients
+  * @typedef    {Object} PatientConfig
+  * @property   {string} id - The instance identifier.
+  * @property   {number} heartbeatPeriod - The time interval, in seconds, between heartbeat events.
+  * @property   {?number} ttl - The patient's time-to-live (in seconds). If null or undefined, the patient 
+  * is deemed immortal.
+  **/
+
+/**
+ * Starts a set of patients according to the given configuration information.
+ * @param {PatientConfig[]} configInfo - The configuration info.
+ */
+function startPatients(configInfo) {
+
+    const patientFactory = require('./patient.js')
+    const channel = { post: (event) => console.log(event) }
+    
+    configInfo
+        .map(element => {
+            return {
+                patient: patientFactory(channel, element.id, element.heartbeatPeriod),
+                ttl: element.ttl
+            }
+        })
+        .forEach(patientInfo => {
+            patientInfo.patient.start()
+            if (patientInfo.ttl)
+                setTimeout(() => patientInfo.patient.stop(), patientInfo.ttl * 1000)
+        })    
 }
 
-const configInfo = JSON.parse(fs.readFileSync(configFilePath).toString())
+/**
+ * The application's entry point.
+ * @param {string} configFilePath - The fully qualified name of the configuration file. 
+ */
+function main(configFilePath) {
 
-const channel = {
-    post: (event) => console.log(event)
-}
+    const fs = require('fs')
 
-configInfo
-    .map(element => {
-        return {
-            patient: patientFactory(channel, element.id, element.heartbeatPeriod),
-            ttl: element.ttl
+    if (!configFilePath || !fs.existsSync(configFilePath)) {
+        console.error(`Could not find file ${configFilePath}`)
+        console.error('Usage: node app <configFilePath>')
+        return
+    }
+
+    fs.readFile(configFilePath, (err, data) => {
+        if(err) {
+            console.error(`Could not read from file ${configFilePath}`)
+            return
         }
+    
+        startPatients(JSON.parse(data.toString()))
     })
-    .forEach(patientInfo => {
-        patientInfo.patient.start()
-        if (patientInfo.ttl)
-            setTimeout(() => patientInfo.patient.stop(), patientInfo.ttl * 1000)
-    })
+}
