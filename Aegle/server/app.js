a6970af256b2949958a5b9402089374dea76c5b1
Paulo Pereira <palbp.giga@gmail.com>
2018-03-29 12:41:55 +0100

    Limpei o entry point da aplicação web

--- Aegle/server/app.js
+++ Aegle/server/app.js
@@ -10,15 +10,16 @@ main(process.argv[2])
 
 /**
  * The application's entry point.
- * @param {string?} port - Optionaly, the port where the server will accept requests
+ * @param {string} port - The port where the server will accept requests
+ * app is being launched in debug mode
  */
 function main(port) {
 
     const model = require('./src/datatypes')
 
     const app = require('./src/routes')
-    const repo = require('./src/patients_repo')
-    const server = app(repo.createRepository(), __dirname)
+    const repo = require('./src/patients_repo').createRepository()
 
+    const server = app(repo, __dirname)
     server.listen(Number(port), () => console.log(`Server is listening on port ${port}`))
 }
